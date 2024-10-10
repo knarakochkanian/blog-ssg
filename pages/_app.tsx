5bdd6e713ec51efcd243b2092d39956c5314fad6
Knara <73983538+knarakochkanian@users.noreply.github.com>
2024-10-10 23:40:40 +0400

    install fix build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import ThemeProvider from '../pages/context/ThemeContext';
-
+import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }: AppProps) => (
     <ThemeProvider>
         <Component {...pageProps} />
